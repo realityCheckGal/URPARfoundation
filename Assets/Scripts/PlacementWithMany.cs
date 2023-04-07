8ccd0cfab01a65e11b12ab23e25364deb0660cf2
Susan Oslin <suzanoslin@gmail.com>
2023-04-06 19:07:24 -0700

    Added Dilmer's Raycast Select code -- won't compile
    Was able to play and build the initial tutorial from Soomeen Hahm, but when I tried to add in Dilmers raycast select code I can't build. And it isn't working in the editor as well.
    
    See READ.ME file for details.

---
+++ Assets/Scripts/PlacementWithMany.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlacementWithMany : MonoBehaviour
+{
+    [SerializeField]
+    private PlacementObject[] placedObjects;
+
+    [SerializeField]
+    private Color activeColor = Color.red;
+
+    [SerializeField]
+    private Color inactiveColor = Color.gray;
+
+    [SerializeField]
+    private Camera arCamera;
+
+    private Vector2 touchPosition = default;
+
+    void Start()
+    {
+        ChangeSelectedObject(placedObjects[0]);
+    }
+
+
+    void Update()
+    {
+        // do not capture events unless the welcome panel is hidden
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    PlacementObject placementObject = hitObject.transform.GetComponent<PlacementObject>();
+                    if (placementObject != null)
+                    {
+                        ChangeSelectedObject(placementObject);
+                    }
+                }
+            }
+        }
+    }
+
+    void ChangeSelectedObject(PlacementObject selected)
+    {
+        foreach (PlacementObject current in placedObjects)
+        {
+            MeshRenderer meshRenderer = current.GetComponent<MeshRenderer>();
+            if (selected != current)
+            {
+                current.Selected = false;
+                meshRenderer.material.color = inactiveColor;
+            }
+            else
+            {
+                current.Selected = true;
+                meshRenderer.material.color = activeColor;
+            }
+
+            //if (displayOverlay) current.ToggleOverlay();
+        }
+    }
+}
